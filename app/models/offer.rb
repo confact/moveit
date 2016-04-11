4d8001ac5072d6582ecb818554dc4e47b94333cd
Håkan Nylén <confacted@gmail.com>
2016-04-11 20:02:20 +0200

    fixed validations

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,8 +1,8 @@
 class Offer < ActiveRecord::Base
   validates :email, email: true
   validates_numericality_of :distance, only_integer: true, greater_than_or_equal_to: 1
-  validates_numericality_of :living_area, only_integer: true, greater_than_or_equal_to: 1
-  validates_numericality_of :storage_area, only_integer: true, greater_than_or_equal_to: 1
+  validates_numericality_of :living_area, only_integer: true, greater_than_or_equal_to: 0
+  validates_numericality_of :storage_area, only_integer: true, greater_than_or_equal_to: 0
   validates :name, presence: true, allow_blank: false
   validates :from_address, presence: true, allow_blank: false
   validates :to_address, presence: true, allow_blank: false
